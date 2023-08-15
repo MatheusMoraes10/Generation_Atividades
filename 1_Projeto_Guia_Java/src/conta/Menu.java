4314ab9a360c6ba748fa11d061edec7c9042fdfa
unknown <moraes.compras@hotmail.com>
2023-08-15 19:35:03 -0300

    Upload projeto Guia class controller

---
+++ 1_Projeto_Guia_Java/src/conta/Menu.java
@@ -0,0 +1,142 @@
+package conta;
+
+import java.util.Scanner;
+
+import conta.controller.ContaController;
+import conta.model.ContaCorrente;
+import conta.model.ContaPoupanca;
+import conta.util.Cores;
+
+public class Menu {
+
+	public static Scanner leia = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+
+		ContaController contas = new ContaController();
+		
+		Scanner leia = new Scanner(System.in);
+		
+		int opcao, numero, agencia, tipo, aniversario;
+		String titular;
+		float saldo, limite;
+		
+		System.out.println("\nCriar Contas\n");
+		
+		ContaCorrente cc1 = new ContaCorrente(contas.gerarNumero(), 123, 1, "João da Silva", 1000f, 100.0f);
+		contas.cadastrar(cc1);
+		
+		ContaCorrente cc2 = new ContaCorrente(contas.gerarNumero(), 124, 1, "Maria da Silva", 2000f, 100.0f);
+		contas.cadastrar(cc2);
+		
+		ContaPoupanca cp1 = new ContaPoupanca(contas.gerarNumero(), 125, 2, "Mariana dos Santos", 4000f, 12);
+		contas.cadastrar(cp1);
+		
+		ContaPoupanca cp2 = new ContaPoupanca(contas.gerarNumero(), 125, 2, "Juliana Ramos", 8000f, 15);
+		contas.cadastrar(cp2);
+		
+		
+		while (true) {
+			
+			System.out.println(Cores.TEXT_YELLOW + Cores.ANSI_BLACK_BACKGROUND + "*****************************************************");
+			System.out.println("                                                     ");
+			System.out.println("                BANCO DO BRAZIL COM Z                ");
+			System.out.println("                                                     ");
+			System.out.println("*****************************************************");
+			System.out.println("                                                     ");
+			System.out.println("            1 - Criar Conta                          ");
+			System.out.println("            2 - Listar todas as Contas               ");
+			System.out.println("            3 - Buscar Conta por Numero              ");
+			System.out.println("            4 - Atualizar Dados da Conta             ");
+			System.out.println("            5 - Apagar Conta                         ");
+			System.out.println("            6 - Sacar                                ");
+			System.out.println("            7 - Depositar                            ");
+			System.out.println("            8 - Transferir valores entre Contas      ");
+			System.out.println("            9 - Sair                                 ");
+			System.out.println("                                                     "); 
+			System.out.println("*****************************************************");
+			System.out.println("Entre com a opção desejada:                          ");
+			System.out.println("                                                     ");
+			opcao = leia.nextInt();
+			
+			if(opcao == 9) {
+				System.out.println("\nBanco do Brazil com Z - O seu futuro começa aqui!");
+				leia.close();
+				System.exit(0);
+			}
+			
+			switch(opcao) {
+			case 1:
+				System.out.println(Cores.TEXT_WHITE + "\n Criar Conta");
+					
+				System.out.println("Digite o Numero da Agência: ");
+				agencia = leia.nextInt();
+				System.out.println("Digite o Nome do Titular: ");
+				leia.skip("\\R?");
+				titular = leia.nextLine();
+				
+				do {
+					System.out.println("Digite o tipo de conta (1 - CC ou 2 - CP)");
+					tipo = leia.nextInt();
+				}while(tipo < 1 && tipo > 2);
+				
+				System.out.println("Digite o Saldo da Conta (R$): ");
+				saldo = leia.nextFloat();
+				
+				switch(tipo) {
+				case 1 -> {
+					System.out.println("Digite o limite de Crédito (R$): ");
+					limite = leia.nextFloat();
+					contas.cadastrar(new ContaCorrente(contas.gerarNumero(), agencia, tipo, titular, saldo, limite));
+				}
+				case 2 -> {
+					System.out.println("Digite o dia do Aniversario da Conta: ");
+					aniversario = leia.nextInt();
+					contas.cadastrar(new ContaCorrente(contas.gerarNumero(), agencia, tipo, titular, saldo, aniversario));
+				}
+				}
+				
+				keyPress();
+                 break;
+			case 2:
+				System.out.println(Cores.TEXT_WHITE + "\n Listar todas as Contas");
+					contas.listarTodas();
+					keyPress();
+                 break;
+			case 3:
+				System.out.println(Cores.TEXT_WHITE + "\n Buscar Conta por número");
+				
+				break;
+			case 4:
+				System.out.println(Cores.TEXT_WHITE + "\n Atualizar dados da Conta");
+				
+                 break;
+			case 5:
+				System.out.println(Cores.TEXT_WHITE + "\n Apagar Conta");
+				
+                 break;
+			case 6:
+				System.out.println(Cores.TEXT_WHITE + "\n Sacar");
+				
+				break;
+             case 7:
+				System.out.println(Cores.TEXT_WHITE + "\n Depositar");
+				
+				break;
+             case 8:
+				System.out.println(Cores.TEXT_WHITE + "\n Transferir");
+				
+				break;
+			default:
+				System.out.println(Cores.TEXT_WHITE + "\nOpção Inválida");
+                 break;
+			}
+        }
+	}
+
+	private static void keyPress() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
